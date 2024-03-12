0e64d950205f2c910142cf1839961f925dbf3127
ajit.none <ajlazer002@gmail.com>
2024-03-12 23:57:32 +0530

    a

--- client/src/components/details.jsx
+++ client/src/components/details.jsx
@@ -5,33 +5,33 @@ import p1 from '../assets/p1.png'
 function details() {
   return (
     <>
-    <div className="container card-details">
-      <div className="w-1/2 border-gray-50">
-        <img src={p1} alt="" />
+    <div className="container flex justify-center shadow-2xl rounded-3xl bg-3">
+      <div className="w-full border-gray-50">
+        <img src={p1} alt="" className='mt-36'/>
       </div>
-      <div className="w-1/2 border-gray-50">
+      <div className=" border-gray-50">
 <h3>hufwafbwa gfwah gwagwag</h3>
 <div className='text-lg'>O HFWIAN GWGOWJO FGWGWAJGWJA?</div>
-<div>
-<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800 w-2/4 glass">
+<div className='grid grid-cols-2 gap-6 w-2/4'>
+<div className=" p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800  glass br-1">
 <p className='text-cyan-400 py-2'> <span className='bg-cyan-800 text-white'>fwafwafaw </span> gfuwabu gwhaig awi <br /> ojg owagiwa jg wajg</p>
 <div className='text-2xl pb-1'>180/100 , As 19H</div>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eos exercitationem ut sequi! Delectus dolores aspernatur sunt libero id voluptatibus itaque </p>
 </div>
 
-<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800 w-2/4 glass">
+<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800  mt-24 glass br-1">
 <p className='text-cyan-400 py-2'> <span className='bg-cyan-800 text-white'>fwafwafaw </span> gfuwabu gwhaig awi <br /> ojg owagiwa jg wajg</p>
 <div className='text-2xl pb-1'>180/100 , As 19H</div>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eos exercitationem ut sequi! Delectus dolores aspernatur sunt libero id voluptatibus itaque </p>
 </div>
 
-<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800 w-2/4 glass">
+<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800  mb-24 glass br-1">
 <p className='text-cyan-400 py-2'> <span className='bg-cyan-800 text-white'>fwafwafaw </span> gfuwabu gwhaig awi <br /> ojg owagiwa jg wajg</p>
 <div className='text-2xl pb-1'>180/100 , As 19H</div>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eos exercitationem ut sequi! Delectus dolores aspernatur sunt libero id voluptatibus itaque </p>
 </div>
 
-<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800 w-2/4 glass">
+<div className="p-4 rounded-tr-3xl rounded-bl-3xl border border-l-red-800  glass br-1">
 <p className='text-cyan-400 py-2'> <span className='bg-cyan-800 text-white'>fwafwafaw </span> gfuwabu gwhaig awi <br /> ojg owagiwa jg wajg</p>
 <div className='text-2xl pb-1'>180/100 , As 19H</div>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam eos exercitationem ut sequi! Delectus dolores aspernatur sunt libero id voluptatibus itaque </p>
