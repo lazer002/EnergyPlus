0a2066e00f447d58baa0f93a70f60f9311089c2f
ajit.none <ajlazer002@gmail.com>
2024-03-17 17:23:12 +0530

    a

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -5,8 +5,8 @@ function footer() {
     <>
     <div className="container pt-35 pb-12">
         <div className="flex justify-between">
-            <div className="utiles">
-<h1>UTILITIES</h1>
+            <div className="utiles space-y-6 font-thin">
+<div className='font-extrabold text-xl'>UTILITIES</div>
 <div>FAQS</div>
 <div>CONTACT US</div>
 <div>PRIVACY POLICY</div>
@@ -14,16 +14,16 @@ function footer() {
 <div>TERMS OF USE</div>
 
             </div>
-            <div className="utiles">
-                <h1>
+            <div className="utiles w-1/3">
+                <div className='font-bold py-4'>
                 DON'T MISS A DROP
-                </h1>
+                </div>
 <div>
-<input type="text" name="" id="" />
-<button type="submit">Signup</button>
+<input type="text" name="" id="" className='p-3 rounded-tl-2xl rounded-bl-2xl w-2/3' />
+<span type="submit" className='bg-slate-100 p-3 rounded-tr-2xl rounded-br-2xl w-32 text-center cursor-pointer'>Signup</span>
 
 </div>
- <div className="flex justify-between">
+ <div className="flex justify-between pt-5 pe-12">
 <img src={suger} alt="" width={24}/>
 <img src={suger} alt="" width={24}/>
 <img src={suger} alt="" width={24}/>
--- client/src/components/fourtwo.jsx
+++ client/src/components/fourtwo.jsx
@@ -5,19 +5,22 @@ function fourtwo() {
   return (
     <>
       <div className="grid grid-cols-2">
-        <div className="border p-12">
+        <div className="border">
 
 
 
 
 
 
-          <div className="">
-            <div className=" relative">
-              <img src={p1} alt="" className='imagehover' />
+          <div className="relative overflow-hidden cardholder">
+            <div className="imagehover flex justify-center  py-12">
+              <img src={p1} alt="" className='size-96 drop-shadow-lg' />
               <div className='prohover '></div>
             </div>
-
+            <img src={p1} alt="" className='size-64 drop-shadow-lg absolute top-4 right-0 rotate-45 opacity-0 sub-picture2' />
+            <img src={p1} alt="" className='size-48 absolute top-0 rotate-15 opacity-0 sub1 drop-shadow-lg' />
+            <img src={p1} alt="" className='size-36 absolute bottom-0 left-24 rotate-45 opacity-0 sub-picture drop-shadow-lg' />
+            <div className='absolute z-10 bottom-24 left-24 hover:left-56 text-8xl font-extrabold cardtext opacity-0 '>ORANGERUN</div>
           </div>
 
 
@@ -28,7 +31,43 @@ function fourtwo() {
 
         </div>
         <div className="border">a</div>
+        <div className="border">a</div>
+        <div className="border">
 
+
+        <div className="relative overflow-hidden cardholder">
+            <div className="imagehover flex justify-center  py-12">
+              <img src={p1} alt="" className='size-96 drop-shadow-lg' />
+              <div className='prohover '></div>
+            </div>
+            <img src={p1} alt="" className='size-64 drop-shadow-lg absolute top-4 right-0 rotate-45 opacity-0 sub-picture2' />
+            <img src={p1} alt="" className='size-48 absolute top-0 rotate-15 opacity-0 sub1 drop-shadow-lg' />
+            <img src={p1} alt="" className='size-36 absolute bottom-0 left-24 rotate-45 opacity-0 sub-picture drop-shadow-lg' />
+            <div className='absolute z-10 bottom-24 left-24 hover:left-56 text-8xl font-extrabold cardtext opacity-0 '>ORANGERUN</div>
+          </div>
+
+
+
+
+        </div>
+        <div className="border">
+        <div className="relative overflow-hidden cardholder">
+            <div className="imagehover flex justify-center  py-12">
+              <img src={p1} alt="" className='size-96 drop-shadow-lg' />
+              <div className='prohover '></div>
+            </div>
+            <img src={p1} alt="" className='size-64 drop-shadow-lg absolute top-4 right-0 rotate-45 opacity-0 sub-picture2' />
+            <img src={p1} alt="" className='size-48 absolute top-0 rotate-15 opacity-0 sub1 drop-shadow-lg' />
+            <img src={p1} alt="" className='size-36 absolute bottom-0 left-24 rotate-45 opacity-0 sub-picture drop-shadow-lg' />
+            <div className='absolute z-10 bottom-24 left-24 hover:left-56 text-8xl font-extrabold cardtext opacity-0 '>ORANGERUN</div>
+          </div>
+
+
+
+        </div>
+<div className="border">
+
+</div>
       </div>
 
 
