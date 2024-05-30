bd874474825b33df664f832933ac909de49a19e3
lazer <ajlazer002@gmail.com>
2024-05-30 23:44:09 +0530

    landing page work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,9 +9,13 @@ function Navbar() {
     setIsOpen(!isOpen);
   }
 
+  function closeMenu() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="flex justify-center">
-      <nav className='p-2 px-4 bg-white rounded-full md:flex md:items-center md:justify-between absolute z-10 w-3/4'>
+      <nav className='p-2 px-4 bg-white rounded-full md:flex md:items-center md:justify-between absolute top-8 z-10 w-3/4'>
         <div className='my-3 flex justify-between items-center z-40'>
           { !isOpen && (
             <span className='text-2xl' onClick={toggleMenu}>
@@ -26,19 +30,16 @@ function Navbar() {
         <div className='text-2xl py-3'>
           <span className='mx-4'>cart</span>
           <span>user</span>
-
-
         </div>
         
-        
-        <ul className={`bg-2 pl-5 ${isOpen ? 'opacity-100' : 'opacity-0'} transition-all ease-in duration-700 bg-slate-200 navtbs`} style={{ transform: 'translateX(-50%)' }}>
-          <li className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/">Home</NavLink></li>
-          <li className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Product">Product</NavLink></li>
-          <li className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/About">About</NavLink></li>
-          <li className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Contact">Contact</NavLink></li>
-          <li className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Signup">Signup</NavLink></li>
+        <div className={`bg-2 pl-5 ${isOpen ? 'flex' : 'hidden'} transition-all ease-in duration-700 bg-slate-200 navtbs `} >
+          <div className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/" onClick={closeMenu}>Home</NavLink></div>
+          <div className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Product" onClick={closeMenu}>Product</NavLink></div>
+          <div className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/About" onClick={closeMenu}>About</NavLink></div>
+          <div className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Contact" onClick={closeMenu}>Contact</NavLink></div>
+          <div className='m-4 hover:text-cyan-500 text-xl'><NavLink to="/Signup" onClick={closeMenu}>Signup</NavLink></div>
           <span className='text-2xl absolute right-28 top-11' onClick={toggleMenu}> X </span>
-        </ul>
+        </div>
       </nav>
     </div>
   );
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,64 +1,56 @@
-import React from 'react'
-import suger from '../assets/Sugar.avif'
-import Footer from './footer'
+import React from 'react';
+import suger from '../assets/p2.png';
+import Footer from './footer';
 
 function Product() {
   return (
- <>
- <div className="grid grid-cols-4 mt-24 text-center">
- <div className="border text-xl">HOME - SNEAKER</div>
- <div className="border col-span-3 py-6">
-  <div className="text-left text-6xl px-10">Search...</div>
+    <>
+ <div className=' my-48 z-40'>
+ <div className="flex justify-between text-center relative">
+        <div className="border w-1/4 text-xl">HOME - SNEAKER</div>
+        <div className="border w-3/4 py-6">
+          <div className="text-left text-6xl px-10">Search...</div>
+        </div>
+      </div>
+      <div className="flex justify-start">
+        <div className="border w-1/4 text-center relative">
+          <div className="text-3xl mt-12">FILTER</div>
+          <div className="filter p-8">
+            <div className="p-2 border text-2xl">Filter 1</div>
+            <div className="p-2 border text-2xl">Filter 2</div>
+            <div className="p-2 border text-2xl">Filter 3</div>
+            <div className="p-2 border text-2xl">Filter 4</div>
+          </div>
+        </div>  
+        <div className="border w-1/4 p-8">
+          <div className="rounded-2xl glass p-6">
+            <div className="text-start">
+              <div className="text-4xl font-bold">ADIDAS</div>
+              <div className="text-md font-normal">ADIDAS FREE ME LEO faw 2</div>
+            </div>
+            
+              <img src={suger} alt="" className='scale-100 hover:scale-125 ease-in-out transition-all hover:rotate-12 z-50' />
+           
+            <div className="flex justify-between">
+              <div className="text-start">
+                <div className="font-normal text-sm text-green-600">ORIGINAL</div>
+                <div className="font-bold text-xl">ORIGINAL fwafwagw</div>
+              </div>
+              <div>
+                <div className="text-4xl font-bold">$130</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div className="border w-1/4">a</div>
+        <div className="border w-1/4">a</div>
+      </div>
  </div>
-  <div className="border">
-    <div className="text-3xl mt-12">FILTER</div>
-<div className="filter p-8">
-  <div className=" p-2 border text-2xl">FIlrter 1</div>
-  <div className=" p-2 border text-2xl">FIlrter 1</div>
-  <div className=" p-2 border text-2xl">FIlrter 1</div>
-  <div className=" p-2 border text-2xl">FIlrter 1</div>
-</div>
-
-  </div>
-  <div className="border p-8 ">
-
-<div className="rounded-2xl glass p-6">
-
-    <div className="text-start">
-<div className="text-4xl font-bolder">ADIDAS</div>
-<div className="text-md font-normal">ADIDAS FREE ME LEO faw 2</div>
-</div>
-<img src={suger} alt="" width={700} className='hover:scale-125 ease-in-out transition-all hover:rotate-12' />
-<div className="flex justify-between">
-  <div className="text-start">
-    <div className='font-normal text-sm text-green-600'>ORIGNAL</div>
-    <div className='font-bolder text-xl'>ORIGNAL fwafwagw</div>
-  </div>
-  <div>
-    <div className='text-4xl font-bolder'>$130</div>
-  </div>
-</div>
-  </div>
-  </div>
-  <div className="border">a</div>
-  <div className="border">a</div>
- </div>
- 
- 
- 
- 
- 
- 
- 
- 
- <Footer/>
- </>
-
-
-
-
-
-  )
+      <Footer />
+    </>
+  );
 }
 
-export default Product
+export default Product;
